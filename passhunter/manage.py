c4aead2836aab041dad713b41c2b2b8489de2d4b
Lukas Dolansky <dolanskyl2@gmail.com>
2025-04-19 11:35:31 +0200

    Add docker PG database

---
+++ passhunter/manage.py
@@ -0,0 +1,16 @@
+"""Command-line interface for managing the application."""
+from flask.cli import FlaskGroup
+
+from app import create_app
+from app.extensions import db
+
+cli = FlaskGroup(create_app())
+
+@cli.command("create_db")
+def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+if __name__ == "__main__":
+    cli()
