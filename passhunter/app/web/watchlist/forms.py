3f9d1c650ba2cda4194f7728748766fdf0c6da6e
Lukas Dolansky <dolanskyl2@gmail.com>
2025-04-21 16:56:56 +0200

    Add watchlists

--- passhunter/app/web/watchlist/forms.py
+++ passhunter/app/web/watchlist/forms.py
@@ -1,26 +1,41 @@
 """Forms for the watchlist page of the PassHunter web application."""
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, BooleanField
-from wtforms.validators import DataRequired, Email, Optional
+from wtforms.fields.simple import EmailField, URLField
+from wtforms.validators import DataRequired, Email, Optional, URL, Length
 
 
 class WatchlistForm(FlaskForm):
     """Form for creating and editing watchlists."""
-    name = StringField('Name', validators=[DataRequired()])
-    description = TextAreaField('Description', validators=[Optional()])
-    is_active = BooleanField('Active', default=True, description='Whether the watchlist is active or not. Deactivated watchlists are not monitored.')
-    mail_address = StringField('Email', validators=[Optional(), Email()], description='Email address to send alerts to.')
-    mail_alerts = BooleanField('Mail Alerts', default=True, description='Whether to send email alerts for this watchlist.')
+    name = StringField('Name', validators=[DataRequired(message='This field must not be empty.'), Length(max=255,
+                                                                                                         message='The maximal length of accepted input is 255 characters.')],
+                       description='Name of your watchlist')
+    description = TextAreaField('Description', validators=[Optional()],
+                                description='Meaningful description of the watchlist.')
+    is_active = BooleanField('Active', default=True,
+                             description='Whether the watchlist is active or not. Deactivated watchlists are not monitored.')
+    email = EmailField('Email', validators=[Optional(), Length(max=255,
+                                                               message='The maximal length of accepted input is 255 characters.'),
+                                            Email(message='Input must adhere to email structure.')],
+                       description='Email address to send alerts to.')
+    send_alerts = BooleanField('Mail Alerts', default=True,
+                               description='Whether to send email alerts for this watchlist.')
     submit = SubmitField('Save')
+
 
 class DeleteWatchlistForm(FlaskForm):
     """Form for deleting a watchlist."""
     submit = SubmitField('Delete')
 
+
 class AddDomainForm(FlaskForm):
     """Form for adding a domain to a watchlist."""
-    domain = StringField('Domain', validators=[DataRequired()])
+    domain = URLField('Domain', validators=[DataRequired(message='This field must not be empty.'), Length(max=255,
+                                                                                                          message='The maximal length of accepted input is 255 characters.'),
+                                            URL(require_tld=False, message='Monitored domain must be a URL address.')],
+                      description='Domain to add to the watchlist.')
     submit = SubmitField('Add Domain')
+
 
 class RemoveDomainForm(FlaskForm):
     """Form for removing a domain from a watchlist."""
