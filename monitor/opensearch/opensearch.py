371e29e72e76bde9b2d358acc1f72eec8be4c954
Lukas Dolansky <dolanskyl2@gmail.com>
2025-05-07 21:29:44 +0200

    Monitor: Add ingest pipeline and search

--- monitor/opensearch/opensearch.py
+++ monitor/opensearch/opensearch.py
@@ -5,60 +5,15 @@ from typing import Any
 from opensearchpy import OpenSearch
 
 
-# TODO chunking does not work
-# Define the ingest pipeline
-txt_pipeline_body = {
-  "description": "Extract attachment information and chunk text",
-  "processors": [
-    {
-      "attachment": {
-        "field": "data",
-        "target_field": "attachment"
-      }
-    },
-    {
-      "set": {
-        "field": "document_content",
-        "value": "{{attachment.content}}"
-      }
-    },
-    {
-      "text_chunking": {
-        "algorithm": {
-          "delimiter": {
-            "delimiter": 'n',
-            "max_chunk_limit": -1
-          }
-        },
-        "field_map": {
-          "document_content": "passage_chunk"
-        }
-      }
-    },
-    {
-      "remove": {
-        "field": "attachment"
-      }
-    },
-    {
-      "remove": {
-        "field": "document_content"
-      }
-    }
-  ]
-}
-
-
-
 class Client(OpenSearch):
     """Custom client for OpenSearch with added shortcut functions."""
-    def __init__(self, init_pipeline: str = 'txt_pipeline', init_index: str = 'documents'):
+    def __init__(self, pipeline_id: str = 'attachment_pipeline', index_id: str = 'infostealers'):
         """
         Initialize the OpenSearch client.
 
         Args:
-            init_pipeline (str): The name of the ingest pipeline to initialize.
-            init_index (str): The name of the index to initialize.
+            pipeline_id (str): The name of the ingest pipeline to initialize.
+            index_id (str): The name of the index to initialize.
         """
         super().__init__(
             hosts=[{'host': os.getenv("OPENSEARCH_HOST", 'localhost'), "port": os.getenv("OPENSEARCH_PORT", 9200)}],
@@ -70,31 +25,161 @@ class Client(OpenSearch):
             ssl_show_warn = False,
         )
         # Create the ingest pipeline
-        if init_pipeline:
-            self.ingest.put_pipeline(id=init_pipeline, body=txt_pipeline_body)
+        self.pipeline_id = pipeline_id
+        self._create_index_pipeline()
         # Set the index
-        self.set_index = init_index
+        self.index_id = index_id
 
-    def search_term(self, search_term: str, search_index: str = None) -> Any:
+    def upload_file(self, encoded_data: str, filename: str, uploaded_at: str) -> Any:
+        """
+        Upload a file to OpenSearch.
+
+        Args:
+            encoded_data (str): The encoded data.
+            filename (str): The filename.
+            uploaded_at (str): The uploaded at date.
+
+        Returns:
+            Response from OpenSearch.
+        """
+        # Prepare the document to be uploaded
+        document = self._prepare_document(encoded_data, filename, uploaded_at)
+        return self.index(index=self.index_id, pipeline=self.pipeline_id, body=document)
+
+    def prepare_bulk_upload(self, encoded_data: str, filename: str, uploaded_at: str) -> dict:
+        """
+        Prepare a bulk indexing action.
+
+        Args:
+            encoded_data (str): The encoded data.
+            filename (str): The filename.
+            uploaded_at (str): The uploaded at date.
+
+        Returns:
+            dict: The prepared bulk action.
+        """
+        return {
+            '_op_type': 'index',
+            '_index': self.index_id,
+            'pipeline': self.pipeline_id,
+            '_source': self._prepare_document(encoded_data, filename, uploaded_at)
+        }
+
+    def search_term(self, search_term: str, uploaded_at: str = None, search_index: str = None) -> Any:
         """
         Search the index for a given term.
 
         Args:
             search_term (str): The search term.
+            uploaded_at (str): The uploaded at date.
             search_index (str): The index to search.
 
         Returns:
             Response from OpenSearch.
         """
+        search_body = self._prepare_search_body(search_term, uploaded_at)
+
+        # Use the default index if not directly specified
+        index = search_index if search_index else self.index_id
+        # Refresh the indexes before search
+        self.indices.refresh(index=index)
+        # Perform the actual search and return the response
+        return self.search(index=index, body=search_body)
+
+    def _create_index_pipeline(self) -> Any:
+        """
+        Create an index pipeline.
+        The pipeline is used for extracting attachment information and chunking text.
+        Removes fields with the original data after processing.
+
+        Returns:
+            Response from OpenSearch.
+        """
+        # Define the ingest pipeline
+        pipeline_body = {
+            "description": "Extract attachment information and chunk text",
+            "processors": [
+                {
+                    "attachment": {
+                        "field": "data",
+                        "target_field": "attachment"
+                    }
+                },
+                {
+                    "split": {
+                        "field": "attachment.content",
+                        "separator": "\n",
+                        "target_field": "attachment_parts"
+                    }
+                },
+                {
+                    "remove": {
+                        "field": "attachment.content"
+                    }
+                },
+                {
+                    "remove": {
+                        "field": "data"
+                    }
+                },
+            ]
+        }
+        return self.ingest.put_pipeline(id=self.pipeline_id, body=pipeline_body)
+
+    def _prepare_document(self, encoded_data: str, filename: str, uploaded_at: str) -> dict:
+        """
+        Prepare the document to be uploaded.
+        The document is used for uploading to OpenSearch.
+
+        Args:
+            encoded_data (str): The encoded data.
+            filename (str): The filename.
+            uploaded_at (str): The uploaded at date.
+
+        Returns:
+            dict: The prepared document.
+        """
+        # Prepare the document to be uploaded
+        document = {
+            "data": encoded_data,
+            "filename": filename,
+            "uploaded_at": uploaded_at
+        }
+
+        return document
+
+    def _prepare_search_body(self, search_term: str, uploaded_at: str = None) -> dict:
+        """
+        Prepare the search body.
+        The body is used for searching in OpenSearch.
+
+        Args:
+            search_term (str): The search term.
+            uploaded_at (str): The uploaded at date.
+            filename (str): The filename identifier.
+
+        Returns:
+            dict: The prepared search body.
+        """
         # Define search body
         search_body = {
             "query": {
-                "match": {
-                    "passage_chunk.content": search_term
+                "bool": {
+                    "must": {
+                        "match": {
+                            "attachment_parts": search_term
+                        }
+                    }
+                }
+            },
+            "highlight": {
+                "fields": {
+                    "attachment_parts": {}
                 }
             }
         }
 
-        index = search_index if search_index else self.set_index
-        self.indices.refresh(index=index)
-        return self.search(index=index, body=search_body)
+        if uploaded_at:
+            search_body["query"]["bool"]["filter"] = {"term": {"uploaded_at": uploaded_at}}
+
+        return search_body
