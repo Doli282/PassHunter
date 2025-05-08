435dec53bbe9d07b0b384439021c62b2a968ae3a
Lukas Dolansky <dolanskyl2@gmail.com>
2025-05-08 21:42:58 +0200

    Web: Add alert count to watchlist view

--- passhunter/app/repository/alert.py
+++ passhunter/app/repository/alert.py
@@ -1,5 +1,37 @@
 """Repository for Alerts"""
+from sqlalchemy import and_
+
+from app import db
+from models import Alert
 
 
-def get_by_domain_id(domain_id: int):
-    return
+def get_count_by_watchlist(watchlist_id: int) -> int:
+    """
+    Return the number of alerts for a watchlist.
+
+    Args:
+        watchlist_id (int): Watchlist ID.
+
+    Returns:
+        int: Number of alerts.
+    """
+    return db.session.scalar(db.select(db.func.count(Alert.id)).filter(Alert.watchlist_id == watchlist_id))
+
+def get_count_by_domain_and_watchlist(domain_id: int, watchlist_id: int) -> int:
+    """
+    Return the number of alerts for a domain and watchlist.
+
+    Args:
+        domain_id (int): Domain ID.
+        watchlist_id (int): Watchlist ID.
+
+    Returns:
+        int: Number of alerts.
+    """
+    return db.session.scalar(db.select(db.func.count(Alert.id)).filter(and_(Alert.domain_id == domain_id, Alert.watchlist_id == watchlist_id)))
+
+def get_alert_count(watchlist_id: int, domain_id: int = None) -> int:
+    if domain_id:
+        return get_count_by_domain_and_watchlist(domain_id, watchlist_id)
+    else:
+        return get_count_by_watchlist(watchlist_id)
