e847c4f6e2f65f1d9eac6e3a58c9dc6183c7ef30
Lukas Dolansky <dolanskyl2@gmail.com>
2025-05-08 20:03:37 +0200

    Web: Show the number of uploaded files on the home page

--- passhunter/app/web/index/routes.py
+++ passhunter/app/web/index/routes.py
@@ -1,8 +1,8 @@
 """Views for the index page of the PassHunter web application."""
 from flask import render_template
 
+from app.repository import file as file_repository
 from app.web.index import bp
-
 
 @bp.route('/')
 @bp.route('/index')
@@ -13,4 +13,5 @@ def index() -> str:
     Returns:
         str: The rendered index page.
     """
-    return render_template('index.html')
+    no_files = file_repository.get_files_count()
+    return render_template('index.html', no_files=no_files)
