4f563ea3026e915339893cdc9dcf9a11fbc649c2
Lukas Dolansky <dolanskyl2@gmail.com>
2025-05-10 10:57:30 +0200

    Extractor: Use patool for extraction

--- extractor/extractor.py
+++ extractor/extractor.py
@@ -1,12 +1,11 @@
 """Extractor - extracts files from archives"""
 import logging
 import os
+import shutil
+import tempfile
 from tempfile import mkdtemp
 
 import patoolib
-import rarfile
-import zipfile
-import py7zr
 
 from celery import Celery
 from celeryconfig import ConfigUploader, ConfigDownloader
@@ -31,166 +30,84 @@ DOWNLOAD_PATH = "/tmp/downloads"
 UPLOAD_PATH = "/tmp/uploads"
 
 @downloader.task(name='extractor.extract_archive')
-def extract_archive(archive_name: str, archive_password: str) -> None:
-    logging.info(f"Received request to extract '{archive_name}' with password '{archive_password}'")
-    archive_path = os.path.join(DOWNLOAD_PATH, archive_name)
-
-    # Extract the archive into a new directory
-    destination = mkdtemp(dir=UPLOAD_PATH)
-    logging.debug(f"Extracting archive to '{destination}'")
-
-    try:
-        # Choose the extractor function based on the archive extension
-        logging.debug(f"Archive extension: {os.path.splitext(archive_name)[-1]}")
-        match os.path.splitext(archive_name)[-1]:
-            case '.zip':
-                extract_zip(archive_path, archive_password, destination)
-            case '.rar':
-                extract_rar(archive_path, archive_password, destination)
-            case _:
-                raise ValueError(f"Unsupported archive type '{archive_name}'")
-        logging.info(f"Archive extracted")
-    except Exception as e:
-        logging.error(f"Failed to extract archive: {e}")
-
-    try:
-        # Send a message to the uploading queue
-        uploader.send_task('uploader.process_batch', args=[os.path.basename(destination)])
-    except Exception as e:
-        logging.error(f"Failed to send message to the uploading queue: {e}")
-
-    # Remove the processed archive
-    #os.remove(archive_name)
-    logging.info(f"Extractor has finished.")
-    return
-
-def extract_patool(archive_path: str, archive_password:str, extract_to: str) -> None:
-    logging.debug("Extracting patool archive.")
-    patoolib.extract_archive(archive_path, verbosity=1, outdir=extract_to, interactive=False, password=archive_password)
-
-def extract_7z(archive_path: str, archive_password:str, extract_to: str) -> None:
-    logging.debug("Extracting 7z archive.")
-    with py7zr.SevenZipFile(archive_path, mode='r', password=archive_password) as archive:
-        all_files = archive.getnames()
-        selected_files = [f for f in all_files if
-                          _filename_filter(f)] if KEYWORDS else None
-        archive.extract(path=extract_to, targets=selected_files)
-
-def extract_zip(archive_path: str, archive_password:str, extract_to: str) -> None:
+def extract_archive(archive_name: str, archive_password: str, archive_dir: str = DOWNLOAD_PATH, destination_path: str = UPLOAD_PATH, keywords: list[str] = KEYWORDS) -> None:
     """
-    Extract only selected files from the zip archive.
+    Extract an archive.
+    The archive is extracted to a temporary directory and then moved to the destination directory.
 
     Args:
-        archive_path (str): Path to the zip archive.
-        archive_password (str): Password for the zip archive.
-        extract_to (str): Directory to extract the files to.
+        archive_name (str): Name of the archive to extract.
+        archive_password (str): Password for the archive.
+        archive_dir (str): Directory where the archive is stored.
+        destination_path (str): Directory where the extracted files should be stored.
+        keywords (list[str]): List of keywords to filter files by.
 
     Returns:
         None
     """
-    logging.debug("Extracting zip archive.")
-    # Save the name of the archive - use it for naming the extracted files
-    archive_name = os.path.basename(archive_path)
-    logging.debug(f"Archive name: {archive_name}")
+    logging.info(f"Received request to extract '{archive_name}' with password '{archive_password}'")
+    archive_path = os.path.join(archive_dir, archive_name)
 
-    # Open the zip archive
-    with zipfile.ZipFile(archive_path, 'r') as zip_ref:
-        # Iteration variable for naming
-        i = 1
-        logging.debug(f"extracting iteration = {i}")
+    # Create a temporary directory to extract the archive to.
+    with tempfile.TemporaryDirectory(ignore_cleanup_errors = True) as tmpdir:
+        try:
+            # Check if the archive really is a valid archive (according to the used tools).
+            if not patoolib.is_archive(archive_path):
+                raise ValueError(f"Archive '{archive_name}' is not a valid archive")
 
-        # Read information on all files and filter only desired files
-        for file in zip_ref.infolist():
-            logging.debug(f"file = {file.filename}")
-            if _filename_filter(file.filename):
-                try:
-                    logging.debug(f"File '{file.filename}' found, extracting")
+            # Extract the entire archive to the temporary directory
+            patoolib.extract_archive(archive_path, outdir=tmpdir, verbosity=-1, interactive=False, password=archive_password)
 
-                    # Rename the file to flatten to structure
-                    file.filename = f"{archive_name}_{os.path.basename(file.filename)}_{i}"
-                    logging.debug(f"file.filename = {file.filename}")
-                    # Extract the file
-                    if zip_ref.extract(file, extract_to, bytes(archive_password, 'utf-8') if archive_password else None):
-                        logging.debug(f"File '{file.filename}' extracted to {extract_to}")
-                    else:
-                        logging.error(f"Failed to extract file '{file.filename}'")
-                    i += 1
-                except zipfile.BadZipFile as e:
-                    logging.error(f"Bad zip file: {e}")
-                except RuntimeError as e:
-                    logging.error(f"Runtime error (Problem with encryption?): {e}")
-                except Exception as e:
-                    logging.error(f"Unknown error: {e}")
+            # Move significant files to destination directory
+            destination = mkdtemp(dir=destination_path)
 
-def extract_rar(archive_path: str, archive_password:str, extract_to: str) -> bool:
-    """
-    Extract only selected files from the rar archive.
+            # Walk through the extracted archive and pick wanted files.
+            i = 0 # Iteration variable to distinguish files with the same name.
+            for root, _, files in os.walk(tmpdir):
+                # Check if a file is the wanted file (contains significant keywords) and move it to destination, if yes.
+                for filename in files:
+                    if _filename_filter(filename, keywords):
+                        try:
+                            # Flatten the directory structure and rename the file.
+                            dest_filename = f"{archive_name}_{filename}_{i}".replace(' ', '_')
+                            shutil.move(os.path.join(root, filename), os.path.join(destination, dest_filename))
+                            i += 1
+                            logging.debug(f"File '{filename}' moved to '{dest_filename}'")
+                        except Exception as e:
+                            logging.error(f"Failed to move file '{filename}': {e}")
+                            continue
 
-    Args:
-        archive_path (str): Path to the rar archive.
-        archive_password (str): Password for the rar archive.
-        extract_to (str): Directory to extract the files to.
+        # Catch exception caused by patoolib.
+        except patoolib.util.PatoolError as e:
+            logging.error(f"Failed to extract archive: {e}")
+            return
+        # Catch exceptions caused by invalid values.
+        except ValueError as e:
+            logging.error(f"Failed to extract archive: {e}")
+            return
+        # Catch all remaining exceptions.
+        except Exception as e:
+            logging.error(f"Failed to extract archive: {e}")
+            return
+        finally:
+            # Cleanup
+            os.remove(archive_path)
 
-    Returns:
-        bool: True if extraction was successful, False otherwise.
-    """
-    logging.debug("Extracting rar archive.")
-    # Save the name of the archive - use it for naming the extracted files
-    archive_name = os.path.basename(archive_path)
+    logging.info(f"Extractor has finished.")
+    return
 
-    # Open the rar archive
-    with rarfile.RarFile(archive_path) as rar_ref:
-        # Set password if provided
-        if archive_password:
-            rar_ref.setpassword(archive_password)
-        elif rar_ref.needs_password():
-            logging.error("Password required for extraction, but no password provided.")
-            return False
-
-        # Iteration variable for naming
-        i = 1
-
-        # Read information on all files and filter only desired files
-        for file in rar_ref.infolist():
-            if _filename_filter(file.filename):
-                logging.debug(f"File '{file.filename}' found, extracting")
-
-                # Rename the file to flatten to structure
-                file_filename = os.path.basename(file.filename)
-                new_filename = f"{archive_name}_{file_filename}_{i}"
-                out_path = os.path.join(extract_to, new_filename)
-
-                # Rewrite the file outside the archive
-                with rar_ref.open(file) as source, open(out_path, 'wb') as target:
-                    target.write(source.read())
-                    logging.debug(f"File extracted to {out_path}")
-
-                i += 1
-
-    return True
-
-def _filename_filter(filename: str) -> bool:
+def _filename_filter(filename: str, keywords: list[str]) -> bool:
     """
     Filter names of files to extract, according to a list of keywords.
 
     Args:
         filename (str): Name of the file to filter.
+        keywords (list[str]): List of keywords to filter files by.
 
     Returns:
         bool: True if the file should be extracted, False otherwise.
     """
-    return  any(word.lower() in filename.lower() for word in KEYWORDS)
-
-if __name__ == "__main__":
-    logging.info("Extractor started.")
-    archive = "/tmp/downloads/RSLC MIX logs [PUBLIC] 114count.zip"
-    extract_archive("RSLC MIX logs [PUBLIC] 114count.zip",'https://t.me/RedLineStealerLogsCloud')
-    print('-'*10 + 'test archive' + '-'*10)
-    #patoolib.test_archive(archive)
-    print('-'*10 + 'archive format' + '-'*10)
-    print(patoolib.get_archive_format(archive))
-    print('-'*10 + 'formats' + '-'*10)
-    #patoolib.list_formats()
-    print('-'*10 + 'supported formats' + '-'*10)
-    #print(patoolib.supported_formats())
-    #extract_patool("/tmp/downloads/RSLC MIX logs [PUBLIC] 114count.zip",'https://t.me/RedLineStealerLogsCloud', '/tmp/uploads/')
+    # If no keywords are provided, always return True.
+    if keywords is None:
+        return True
+    return  any(word.lower() in filename.lower() for word in keywords)
--- extractor/notes.md
+++ extractor/notes.md
@@ -2,15 +2,46 @@
 
 Dependencies:
 
-sudo apt install unrar
+```shell
+apt install unrar
+apt install p7zip-full
+```
 
 ```shell
-pip install rarfile
+pip install patool
 pip install celery
 ```
 
 ## Extraction
 
-Separate functions for different archive formats.
-Currently, supports `zip` and `rar`.
-It can be extended to support other formats.
+There are two approaches to how to address extraction.
+Either to have a separate function for different archive formats
+or to use some general tool that can distinguish the formats and choose the best tool by itself.
+Secondly, it is more efficient to extract only the important files  
+instead of extracting the whole archive and then pick only the valuable files.
+
+https://rarfile.readthedocs.io/index.html
+https://docs.python.org/3/library/zipfile.html#module-zipfile
+
+Using separate functions for different archive formats would utilize libraries like ZipFile and RarFile.
+The benefits are more efficient use of the library's features and possible customization of individual functions.
+However, they also have their limitations.
+ZipFile and RarFile enable extracting only specific files.
+An extension for other formats means adding another function and another library.
+Moreover, ZipFile does not support AES encryption.
+Thus, despite having the .zip extension, the archive cannot be extracted.
+It knows the directory structure of the archive content, but it is unable to extract the individual files due to an unknown format.
+Hence, this method was deemed unsufficient, since AES-encrypted archives are quite prevalent. 
+
+The second approach is to use a tool that can find the most appropriate tool for the extraction by itself.
+Thanks to this, only a single function is necessary to invoke the extraction.
+On the other hand, these general tools offer only general API functions that are supported by all tools discarding specific features of individual tools.
+Consequently, it is mandatory to first extract the whole archive and then find the relevant files.
+This can lead to a slower performance and higher space usage.
+
+The general tool used in the application is `patool`.
+This tool relies on external tools.
+(Although the same applies for RarFile, too.)
+However, it is also capable of identifying and extracting AES-encrypted archives.
+https://github.com/wummel/patool
+https://wummel.github.io/patool/
