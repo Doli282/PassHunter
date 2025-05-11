ea2ec58fd07d0ec4c9e7a5b0a5d98212c59e268c
Lukas Dolansky <dolanskyl2@gmail.com>
2025-05-12 01:07:39 +0200

    Forwarder: Add links to other channels

--- forwarder/config.py
+++ forwarder/config.py
@@ -16,6 +16,10 @@ class Config(object):
     LOGGING_FORMAT = '%(asctime)s Forwarder: %(levelname)s: %(message)s'
     # Application
     # IDs, usernames or links identifying the source channels
-    SOURCE_CHANNEL_IDS = [-1002283692447, -1002494393489, -1001935880746]
+    SOURCE_CHANNEL_IDS = [-1002283692447, -1001935880746, -1002167657930, -1001828534036]
+    # -1002494393489 ... test channel
+    # -1002167657930 ... https://t.me/MavroCloud
+    # -1001935880746 ... https://boxed.pw
+    # -1001828534036... https://t.me/OBSERVERCLOUD2
     # ID, username or link identifying the target channel
     TARGET_CHANNEL_ID = -1002381035490
