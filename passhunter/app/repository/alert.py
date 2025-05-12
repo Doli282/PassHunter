1078358ca1d39d43d0f4702410bae94c6fd6a1f0
Lukas Dolansky <dolanskyl2@gmail.com>
2025-05-12 02:39:44 +0200

    Web: Change state of alerts

--- passhunter/app/repository/alert.py
+++ passhunter/app/repository/alert.py
@@ -5,7 +5,8 @@ from flask_sqlalchemy.pagination import Pagination
 from sqlalchemy import and_
 
 from app import db
-from models import Alert
+from models import Alert, Watchlist
+
 
 def get_count_by_watchlist(watchlist_id: int) -> int:
     """
@@ -54,3 +55,37 @@ def get_page_all(page: int = 1) -> Pagination:
     # Get all alerts
     query_alerts = db.select(Alert).join(watchlists, Alert.watchlist_id == watchlists.c.id)
     return db.paginate(select=query_alerts, page=page,max_per_page=current_app.config['PER_PAGE'])
+
+def get_alert_by_id(alert_id: int) -> Alert:
+    """
+    Retrieve Alert by ID.
+    Rejects requests for Alerts that do not belong to the current user.
+
+    Args:
+        alert_id (int): Alert ID.
+
+    Returns:
+        Alert: Alert by ID.
+    """
+    query = db.select(Alert).join(Watchlist, Alert.watchlist_id == Watchlist.id).filter(
+        Alert.id == alert_id,
+        Watchlist.account_id == current_user.id
+    )
+    return db.one_or_404(query)
+
+def change_alert_state(alert_id: int) -> Alert:
+    """
+    Switch the state of an alert.
+    Register an alert if it was new.
+    Make the alert new if it was already registered.
+
+    Args:
+        alert_id (int): ID of the alert to register.
+
+    Returns:
+        Alert: Registered alert.
+    """
+    alert = get_alert_by_id(alert_id)
+    alert.is_new = not alert.is_new
+    db.session.commit()
+    return alert
--- passhunter/app/web/alert/routes.py
+++ passhunter/app/web/alert/routes.py
@@ -1,7 +1,8 @@
 """Views for the alert page of the PassHunter web application."""
-from flask import request, render_template
+from flask import request, render_template, redirect, url_for, flash, Response
 
 import app.repository.alert as alert_repository
+from app.web import EmptyForm
 from app.web.alert import bp
 
 @bp.route('/alerts')
@@ -14,4 +15,22 @@ def list_alerts() -> str:
     """
     page = request.args.get('page', 1, type=int)
     pagination = alert_repository.get_page_all(page)
-    return render_template('alert/list.html', pagination=pagination)
+    return render_template('alert/list.html', pagination=pagination, form=EmptyForm())
+
+@bp.route('/alerts/<int:alert_id>/register', methods=['POST'])
+def change_alert_state(alert_id: int) -> Response:
+    """
+    Change the state of an alert.
+    Switch between 'new' and 'registered'.
+
+    Args:
+        alert_id (int): The ID of the alert to change the state of.
+
+    Returns:
+        Response: Redirect to the referrer page or the alert list page.
+    """
+    alert = alert_repository.change_alert_state(alert_id)
+    flash(f"The alert has been set to {alert.print_status()}.")
+    return redirect(request.referrer or  url_for('alert.list_alerts'))
+
+
