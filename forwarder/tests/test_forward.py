12c6ff301782848eb884beced7f183ebe9d5402e
Lukas Dolansky <dolanskyl2@gmail.com>
2025-05-15 00:26:53 +0200

    forwarder: add negative test

--- forwarder/tests/test_forward.py
+++ forwarder/tests/test_forward.py
@@ -8,7 +8,7 @@ from forwarder import Config
 class TestForward(unittest.IsolatedAsyncioTestCase):
 
     async def test_forward(self):
-        """Test the forward function."""
+        """Test that a message with a document is forwarded."""
         # Create a moc message.
         mock_message = AsyncMock()
         mock_message.document = True
@@ -19,4 +19,18 @@ class TestForward(unittest.IsolatedAsyncioTestCase):
         await forward(mock_message)
 
         # Assert the forwarding was called on the message.
-        mock_message.forward_to.assert_called_once_with(int(Config.TARGET_CHANNEL_ID))
+        mock_message.forward_to.assert_awaited_once_with(int(Config.TARGET_CHANNEL_ID))
+
+    async def test_not_forward(self):
+        """Test that a message without a document is not forwarded."""
+        # Create a moc message.
+        mock_message = AsyncMock()
+        mock_message.document = False
+        mock_message.file.name = 'test.txt'
+        mock_message.to_id = 123
+
+        # Run the tested function.
+        await forward(mock_message)
+
+        # Assert the forwarding was not called on the message.
+        mock_message.forward_to.assert_not_awaited()
